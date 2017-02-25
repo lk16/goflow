640e1b1ded477ba689e1f11276f2a30892ee2912
Luuk Verweij <luuk_verweij@msn.com>
2017-02-25 15:16:56 +0100

    using nodes inside board

--- src/flow/board.go
+++ src/flow/board.go
@@ -2,30 +2,40 @@ package flow
 
 import "fmt"
 
+type node struct {
+    neigbours *[]node
+    endpoint bool
+    x,y,colour int
+}
+
 type Board struct {
     width,height int
-    fields []int
+    fields [][]node
 }
 
-func NewBoard(width,height int) *Board {
-    board := Board{width,height,make([]int,width*height)}
+func NewSquareBoard(width,height int) *Board {
+    board := Board{width,height,make([][]node,height)}
+    for i:=0; i<height; i++ {
+        board.fields[i] = make([]node,width)
+    }
     return &board
+
 }
 
-func (board *Board) GetField(x,y int) int {
+func (board *Board) getField(x,y int) node {
     if x < 0 || x >= board.width {
         panic("Accessing x outside bounds")
     } 
     if y < 0 || y >= board.height {
         panic("Accessing y outside bounds")
     }
-    return board.fields[y*board.width + x]
+    return board.fields[y][x]
 }
 
 func (board *Board) Print() {
     for y:=0; y<board.height; y++ {
         for x:=0; x<board.width; x++ {
-            v := board.GetField(x,y)
+            v := board.getField(x,y).colour
             if v == 0 {
                 fmt.Printf(" -")
             } else {
--- src/main.go
+++ src/main.go
@@ -1,11 +1,8 @@
 package main
 
-import "fmt"
 import "flow"
 
-
-
 func main() {
-    board := flow.NewBoard(5,5)
+    board := flow.NewSquareBoard(5,5)
     board.Print()
 }

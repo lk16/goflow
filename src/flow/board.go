6d11da74dd42a20cc8f3e52542b54f6085426f27
Luuk Verweij <luuk_verweij@msn.com>
2017-02-25 18:06:46 +0100

    implemented rectangle print with edges

--- src/flow/board.go
+++ src/flow/board.go
@@ -2,17 +2,22 @@ package flow
 
 import "fmt"
 
-type end struct {
-    node_id [2]int
+type pair [2]int
+
+type Board interface {
+    PrintEdges()
+    Print()
+    Solve()
 }
 
-type Board struct {
-    ends []end
+type RectangleBoard struct {
+    ends []pair
     edges [][]int
+    width,height int
 }
 
-func NewSquareBoard(width,height int) *Board {
-    board := Board{make([]end,0),make([][]int,width*height)}
+func NewRectangleBoard(width,height int) *RectangleBoard {
+    board := RectangleBoard{make([]pair,0),make([][]int,width*height),width,height}
     for i:=0; i<width*height; i++ {
         board.edges[i] = make([]int,0)
     }
@@ -30,15 +35,24 @@ func NewSquareBoard(width,height int) *Board {
         }
     }
     return &board
-
 }
 
-func (board *Board) addEdge(a,b int) {
+func (board *RectangleBoard) AddEnds(a,b int) {
+    if a < 0 || a >= board.width*board.height {
+        panic("Edge index invalid")
+    }
+    if b < 0 || b >= board.width*board.height {
+        panic("Edge index invalid")
+    }
+    board.ends = append(board.ends,pair{a,b})
+}
+
+func (board *RectangleBoard) addEdge(a,b int) {
     board.edges[a] = append(board.edges[a],b)
     board.edges[b] = append(board.edges[b],a)
 }
 
-func (board *Board) Print() {
+func (board *RectangleBoard) PrintEdges() {
     for id,node := range board.edges {
         fmt.Printf("Node (%d): ",id)
         for _,neighbour := range node {
@@ -47,3 +61,88 @@ func (board *Board) Print() {
         fmt.Printf("\n")
     }
 } 
+
+func (board *RectangleBoard) Print() {
+    board.print(make([]pair,0))
+}
+
+func (board *RectangleBoard) print(edges []pair) {
+
+    endpoints := make([]int,board.width*board.height)
+
+    for i,_ := range endpoints {
+        endpoints[i] = -1
+    }
+
+    for id,pair := range board.ends {
+        endpoints[pair[0]] = id
+        endpoints[pair[1]] = id
+    }
+
+    for y:=0; y<board.height; y++ {
+        for x:=0; x<board.width; x++ {
+            
+            has_edge := false
+            id := y*board.width + x
+            for _,edge := range edges {
+                if (edge[0] == id-1) && (edge[1] == id) {
+                    has_edge = true
+                    break
+                }  
+            } 
+            if has_edge {
+                fmt.Printf("---")                
+            } else {
+                fmt.Printf("   ")
+            }
+
+            endpoint_id := endpoints[y*board.width + x]
+            if endpoint_id >= 0 {
+                fmt.Printf("%d",endpoint_id)
+            } else {
+                fmt.Printf("·")
+            }
+        }
+        fmt.Printf("\n")
+
+        for x:=0; x<board.width; x++ {
+            has_edge := false
+            id := y*board.width + x
+            for _,edge := range edges {
+                if (edge[0] == id) && (edge[1] == id+board.width) {
+                    has_edge = true
+                    break
+                }  
+            } 
+            if has_edge {
+                fmt.Printf("---")
+            } else {
+                fmt.Printf("   ")
+            }
+
+        }
+        fmt.Printf("\n")
+    }
+}
+
+type solver struct {
+    board *RectangleBoard
+    edges []pair // smallest id first
+    node_used []bool
+}
+
+func (board *RectangleBoard) Solve() {
+    state := solver{
+        board: board,
+        edges: make([]pair,0),
+        node_used: make([]bool,board.width*board.height)}
+    state.solve()
+}
+
+func (state *solver) print() {
+
+}
+
+func (state *solver) solve() {
+
+}
--- src/main.go
+++ src/main.go
@@ -1,8 +1,14 @@
 package main
 
 import "flow"
+import "fmt"
 
 func main() {
-    board := flow.NewSquareBoard(4,4)
+    board := flow.NewRectangleBoard(4,4)
+    //board.PrintEdges()
+    board.AddEnds(0,15)
+    fmt.Printf("\n")
     board.Print()
+    fmt.Printf("\n")
+    board.Solve()
 }
